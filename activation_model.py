b7be400974c576b0fac18bf2fd5fa7af453bd706
Titania Sugiarto <titania.sugiarto@rwth-aachen.de>
2025-04-07 15:51:52 +0200

    activation and repression model added

---
+++ activation_model.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+import numpy as np
+
+y0 = [0,0]
+t = np.linspace(0, 200, num=100)
+
+# Parameters
+k_1 = 0.5
+gamma_1 = 0.1 #should be smaller than k_1 to reach steady state
+k_2 = 0.5
+gamma_2 = 0.05
+n = 5
+c = 5
+
+params = [k_1, gamma_1, k_2, gamma_2, n, c]
+
+def sim (variables, t, params):
+
+    G1 = variables[0]
+    G2 = variables[1]
+
+    k_1 = params[0]
+    gamma_1 = params[1]
+    k_2 = params[2]
+    gamma_2 = params[3]
+    n = params[4]
+    c = params[5]
+
+    dG1_dt = k_1 - gamma_1 * G1
+    dG2_dt = (G1**n / (c**n + G1**n)) * k_2 - gamma_2 * G2
+
+    return ([dG1_dt, dG2_dt])
+
+y = odeint(sim, y0, t, args=(params,))
+
+fig, ax = plt.subplots(1)
+
+line1, = ax.plot(t, y[:,0], color='b', label='G1')
+line2, = ax.plot(t, y[:,1], color='r', label='G2')
+
+ax.set_ylabel('Number')
+ax.set_xlabel('Time')
+
+ax.legend(handles=[line1,line2])
+
+plt.show()
